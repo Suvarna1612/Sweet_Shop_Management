9c8f731d17e7cd5e62671a5f798479a88d80127d
Suvarna1612 <suvarnat1612@gmail.com>
2025-12-14 17:20:28 +0530

    Handled missing password and invalid emails and refactored code and tests failed for weak password

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-
 app.post("/api/auth/register", (req, res) => {
-
-
-    
-  const { email ,password} = req.body;
+  const { email, password } = req.body;
 
   if (!email || !password) {
     return res.status(400).json({
@@ -16,10 +12,18 @@ app.post("/api/auth/register", (req, res) => {
     });
   }
 
+  const emailFormat = /^\S+@\S+\.\S+$/;
+  if (!emailFormat.test(email)) {
+    return res.status(400).json({
+      message: "Invalid email format",
+    });
+  }
+
   return res.status(201).json({
     token: "token",
   });
 });
+
 
 module.exports = app;
 
--- backend/tests/auth_int.test.js
+++ backend/tests/auth_int.test.js
@@ -35,5 +35,34 @@ it("should fail if password is missing", async () => {
 });
 
 
+it("should fail if email format is invalid", async () => {
+  const res = await request(app)
+    .post("/api/auth/register")
+    .send({
+      email: "invalid-email",
+      password: "password123"
+    });
+
+  expect(res.statusCode).toBe(400);
+});
+
+
+
+it("should fail if password is too weak", async () => {
+  const res = await request(app)
+    .post("/api/auth/register")
+    .send({
+      email: "test@example.com",
+      password: "123"
+    });
+
+  expect(res.statusCode).toBe(400);
+});
+
+
+
+
+
+
 
 });
